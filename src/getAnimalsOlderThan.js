49e7584d2315dd50efd4f94007035baa249b5dc2
nataliaschmidt <nataliak.schmidt@gmail.com>
2023-01-27 10:34:46 -0300

    Requisito 02 Resolvido

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,14 @@
 const data = require('../data/zoo_data');
 
 const getAnimalsOlderThan = (animal, age) => {
-  // seu código aqui
+  const findAnimal = data.species.find((specie) => specie.name === animal);
+  // console.log(findAnimal);
+
+  return findAnimal.residents.every((resident) => resident.age >= age);
 };
+
+// retora true se os animais possuirem a mesma idade ou forem mais velhos
+// retorna false se os animais forem mais novos
+console.log(getAnimalsOlderThan('lions', 2));
 
 module.exports = getAnimalsOlderThan;
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,13 @@
 const data = require('../data/zoo_data');
 
-const getSpeciesByIds = (ids) => {
-  // seu código aqui
+/// https://www.w3docs.com/learn-javascript/rest-parameters-and-spread-syntax.html#:~:text=The%20rest%20parameter%20gives%20you,how%20it%20can%20be%20defined.
+// ... rest parameters "transforma" os parâmetros em um array
+const getSpeciesByIds = (...ids) => {
+  // console.log(ids);
+  if (!ids) return [];
+  return ids.map((id) => data.species.find((specie) => specie.id === id));
 };
+
+console.log(getSpeciesByIds());
 
 module.exports = getSpeciesByIds;
