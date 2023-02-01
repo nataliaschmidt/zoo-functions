6e5af556f92aa54fcb249846707aa77482939228
nataliaschmidt <nataliak.schmidt@gmail.com>
2023-02-01 16:28:44 -0300

    Requisito 14 passando nos testes e precisando ser refatorado

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,91 +1,120 @@
-// const data = require('../data/zoo_data');
-// const { species } = data
+const data = require('../data/zoo_data');
 
-// const allAnimals = () => {
+const { species } = data;
 
-//   return species.reduce((acc, specie) => {
-//     if (!acc[specie.location]) {
-//       acc[specie.location] = []
-//     }
+const allAnimals = () => species.reduce((acc, specie) => {
+  if (!acc[specie.location]) {
+    acc[specie.location] = [];
+  }
 
-//     const filterAnimals = species.filter((animal) => {
-//       return animal.location === specie.location
-//     })
-//     const names = filterAnimals.map((name) => {
-//       return name.name
-//     })
+  const filterAnimals = species.filter((animal) => animal.location === specie.location);
+  const names = filterAnimals.map((name) => name.name);
 
-//     acc[specie.location] = names
-//     return acc
-//   }, {})
-// }
-// // console.log(allAnimals());
+  acc[specie.location] = names;
+  return acc;
+}, {});
+// console.log(allAnimals());
 
-// const includeNames = () => {
+const includeNames = () => species.reduce((acc, specie) => {
+  if (!acc[specie.location]) {
+    acc[specie.location] = [];
+  }
+  const filterAnimals = species.filter((filterAnimal) => filterAnimal.location === specie.location);
+  const nameAnimals = filterAnimals.map((nameAnimal) => {
+    const obj = {};
+    obj[nameAnimal.name] = nameAnimal.residents.map((resident) => resident.name);
+    return obj;
+  });
 
-//   return species.reduce((acc, specie) => {
-//     if (!acc[specie.location]) {
-//       acc[specie.location] = []
-//     }
-// const filterAnimals = species.filter((filterAnimal) => {
-//   return filterAnimal.location === specie.location
-// })
-// const nameAnimals = filterAnimals.map((nameAnimal) => {
-//   let obj = {}
-//    obj[nameAnimal.name] = nameAnimal.residents.map((resident) => {
-// return resident.name
-//    })
-//    return obj
-//   })
+  acc[specie.location] = nameAnimals;
+  return acc;
+}, {});
 
-//     acc[specie.location] = nameAnimals
-//     return acc
-//   }, {})
-// }
+// console.log(includeNames());
 
-//  // console.log(includeNames());
+const sortedTrue = () => species.reduce((acc, specie) => {
+  if (!acc[specie.location]) {
+    acc[specie.location] = [];
+  }
+  const filterAnimals = species.filter((filterAnimal) => filterAnimal.location === specie.location);
+  const nameAnimals = filterAnimals.map((nameAnimal) => {
+    const obj = {};
+    obj[nameAnimal.name] = nameAnimal.residents.map((resident) => resident.name).sort();
+    //  console.log(obj);
+    return obj;
+  });
+  acc[specie.location] = nameAnimals;
+  return acc;
+}, {});
 
-//  const sortedTrue = () => {
-//   return species.reduce((acc, specie) => {
-//     if (!acc[specie.location]) {
-//       acc[specie.location] = []
-//     }
-// const filterAnimals = species.filter((filterAnimal) => {
-//   return filterAnimal.location === specie.location
-// })
-// const nameAnimals = filterAnimals.map((nameAnimal) => {
-//   let obj = {}
-//    obj[nameAnimal.name] = nameAnimal.residents.map((resident) => {
-// return resident.name
-//    }).sort()
-//   //  console.log(obj);
-//    return obj
-//   })
-//     acc[specie.location] = nameAnimals
-//     return acc
-//   }, {})
-//  }
+// console.log(sortedTrue());
 
-// // console.log(sortedTrue());
+// const teste = sortedTrue()
+// console.log(Object.values(teste)[3]);
 
-// // const teste = sortedTrue()
-// // console.log(Object.values(teste)[3]);
+const NameAndSex = (objeto) => species.reduce((acc, specie) => {
+  if (!acc[specie.location]) {
+    acc[specie.location] = [];
+  }
+  const filterAnimals = species.filter((filterAnimal) => filterAnimal.location === specie.location);
+  const nameAnimals = filterAnimals.map((nameAnimal) => {
+    const obj = {};
+    obj[nameAnimal.name] = nameAnimal.residents.reduce((acc1, resident) => {
+      if (resident.sex === objeto.sex) {
+        acc1.push(resident.name);
+      }
+      return acc1;
+    }, []);
+    return obj;
+  });
 
-// const getAnimalMap = (options) => {
-//   if (!options || !options.includeNames) {
-//     return allAnimals()
-//   }
-//   if (options.includeNames === true && options.sorted === true){
-//     return sortedTrue()
-//   }
-// if (options.includeNames === true){
-//   return includeNames()
-// }
+  acc[specie.location] = nameAnimals;
+  return acc;
+}, {});
 
-// };
+// const teste = NameAndSex({ includeNames: true, sex: 'female' });
+// console.log(Object.values(teste)[0]);
 
-// const teste2 = getAnimalMap({ includeNames: true, sorted: true });
+const NameAndSexAndSort = (objeto) => species.reduce((acc, specie) => {
+  if (!acc[specie.location]) {
+    acc[specie.location] = [];
+  }
+  const filterAnimals = species.filter((filterAnimal) => filterAnimal.location === specie.location);
+  const nameAnimals = filterAnimals.map((nameAnimal) => {
+    const obj = {};
+    obj[nameAnimal.name] = nameAnimal.residents.reduce((acc2, resident) => {
+      if (resident.sex === objeto.sex) {
+        acc2.push(resident.name);
+      }
+      return acc2;
+    }, []).sort();
+    return obj;
+  });
+
+  acc[specie.location] = nameAnimals;
+  return acc;
+}, {});
+
+const getAnimalMap = (options) => {
+  if (!options || !options.includeNames) {
+    return allAnimals();
+  }
+  if (options.includeNames === true && !options.sorted && !options.sex) {
+    return includeNames();
+  }
+  if (options.includeNames === true && options.sorted === true && !options.sex) {
+    return sortedTrue();
+  }
+  if (options.includeNames === true && !options.sorted && options.sex) {
+    return NameAndSex(options);
+  }
+  if (options.includeNames === true && options.sorted === true && options.sex) {
+    return NameAndSexAndSort(options);
+  }
+};
+
+// const teste2 = getAnimalMap({ includeNames: true, sex: 'female', sorted: true });
 
 // console.log(Object.values(teste2)[0]);
 
-// module.exports = getAnimalMap;
+module.exports = getAnimalMap;
